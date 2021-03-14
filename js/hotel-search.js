a908c81127ac82ec1bd01f2281723c17b6eac221
Igor Fendrikh <70067771+Woland269@users.noreply.github.com>
2021-03-14 20:37:19 +0500

    Исправил js

--- js/hotel-search.js
+++ js/hotel-search.js
@@ -1,11 +1,35 @@
-const  searchHotelButton = document.querySelector(".finding-hotels");
-const  modalWindow = document.querySelector(".interested-form");
-const  searchHotelClicked = document.querySelector(".search-hotel-clicked");
+const searchHotelButton = document.querySelector('.finding-hotels');
+const modalWindow = document.querySelector('.interested-form');
+const modalWindowWrapper = document.querySelector('.interested-form-wrapper');
 
-searchHotelButton.addEventListener ("click", function () {
+const checkInputsValue = (inputs) => {
+  let flag = true;
+  inputs.forEach((input) => {
+    if (!input.value) {
+      flag = false;
+    }
+  });
+  return flag;
+};
 
-    modalWindow.classList.toggle ("animation-modal");
+if (modalWindowWrapper) {
+  const inputs = modalWindow.querySelectorAll('input');
 
-    modalWindow.classList.toggle ("search-hotel-clicked");
+  modalWindowWrapper.classList.remove('active');
 
-});
+  modalWindow.addEventListener('submit', (e) => {
+    if (!checkInputsValue(inputs)) {
+      e.preventDefault();
+      modalWindow.classList.add('has-error');
+
+      setTimeout(() => {
+        modalWindow.classList.remove('has-error');
+      }, 500);
+    }
+  });
+
+  searchHotelButton.addEventListener('click', function (e) {
+    e.preventDefault();
+    modalWindowWrapper.classList.toggle('active');
+  });
+}
